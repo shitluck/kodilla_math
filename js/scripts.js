86becc74175c7758899b0d7da1191d62372aa5e9
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-03-28 11:38:08 +0200

    few changes in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,18 @@ var value = (a * a) + (2 * a * b) - (b * b);
 alert('Wynik działania wynosi: ' + value);
 console.log('Wynik działania wynosi: ' + value);
 
-if (value > 0) {
-    alert('Wynik jest dodatni');
-    console.log('Wynik dodatni');
-} else if (value < 0) {
-    alert('Wynik jest ujemny');
-    console.log('Wynik ujemny');
+if(isFinite(value) == true) {
+    if (value > 0) {
+        alert('Wynik jest dodatni');
+        console.log('Wynik dodatni');
+    } else if (value < 0) {
+        alert('Wynik jest ujemny');
+        console.log('Wynik ujemny');
+    } else {
+        alert('Wynik działania to zero');
+        console.log('Wynik działania to zero');
+    }
 } else {
-    alert('Wynik działania to zero');
-    console.log('Wynik działania to zero');
+    alert('Wynik działania nie jest liczbą, proszę wprowadzić wartości liczbowe');
+    console.log('Wynik działania nie jest liczbą, proszę wprowadzić wartości liczbowe');
 }
